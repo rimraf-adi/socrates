ec9c073adc563841b17af05d186243eb25b770b3
aditya kinjawadekar <kinjawadekaradi112@gmail.com>
2026-01-31 18:46:11 +0530

    cleanup

--- src/services/GroqClient.ts
+++
@@ -1,9 +0,0 @@
-import { SearchMode } from '../App';
-
-export async function synthesizeAnswer(
-    query: string,
-    context: string,
-    mode: SearchMode
-): Promise<string> {
-    return window.electronAPI.synthesizeAnswer(query, context, mode);
-}
